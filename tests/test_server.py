5282dd267dcc44af92049f7b956dc9fca49acc28
Taylor Gay <taylorjgay@gmail.com>
2026-06-02 12:24:34 -0500

    test: update EXPECTED_TOOLS for ER605 Phase 2 (27 total tools)

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,25 +1,15 @@
 import src.server as server_module
 
 EXPECTED_TOOLS = [
-    "get_wan_status",
-    "get_router_info",
-    "get_connected_clients",
-    "get_mesh_health",
-    "get_pihole_stats",
-    "get_query_log",
-    "get_top_domains",
-    "get_top_clients",
-    "get_domain_lists",
-    "get_clients",
-    "get_pihole_system",
-    "add_domain",
-    "remove_domain",
-    "set_blocking",
-    "update_gravity",
-    "test_dns_resolution",
-    "ping_host",
-    "traceroute_host",
-    "run_speedtest",
+    "get_wan_status", "get_router_info",
+    "get_wan_policy", "set_wan_priority",
+    "get_port_forwards", "add_port_forward", "remove_port_forward",
+    "get_firewall_rules", "add_firewall_rule", "remove_firewall_rule",
+    "get_connected_clients", "get_mesh_health",
+    "get_pihole_stats", "get_query_log", "get_top_domains", "get_top_clients",
+    "get_domain_lists", "get_clients", "get_pihole_system", "add_domain",
+    "remove_domain", "set_blocking", "update_gravity",
+    "test_dns_resolution", "ping_host", "traceroute_host", "run_speedtest",
 ]
 
 
